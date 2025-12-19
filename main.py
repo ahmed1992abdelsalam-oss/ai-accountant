d20c01be3ed93af23dedb24b5d28325a999adea0
ahmed1992abdelsalam-oss <ahmed1992abdelsalam@gmail.com>
2025-12-19 05:55:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ app = FastAPI(title="AI Accountant")
 @app.get("/", response_class=HTMLResponse)
 def home():
     return """
+    <!DOCTYPE html>
     <html>
         <head>
             <title>AI Accountant</title>
         </head>
         <body style="font-family:Arial;text-align:center;margin-top:50px">
             <h1>✅ AI Accountant is running</h1>
-            <p>FastAPI + Render</p>
+            <p>FastAPI + Render deployment successful 🚀</p>
         </body>
     </html>
     """
 
-# مهم جدًا لـ Render
+# مهم جدا لتشغيل Render
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 10000))
